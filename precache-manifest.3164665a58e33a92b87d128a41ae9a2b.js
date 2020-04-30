07081471e9b2dc27c964e448623545b2ffa14ef8
Konrad Chilimoniuk <chilimoniuk.konrad@gmail.com>
2020-04-30 13:00:21 +0200

    Updates

---
+++ precache-manifest.3164665a58e33a92b87d128a41ae9a2b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62286b16892bf912f299e7317dac690a",
+    "url": "/polyline-to-geojson/264d81655d3b71a8148b.worker.js"
+  },
+  {
+    "revision": "111596cac3623bd56a47fc3b1e601011",
+    "url": "/polyline-to-geojson/index.html"
+  },
+  {
+    "revision": "89c3f734ebbff7c8a7ff",
+    "url": "/polyline-to-geojson/static/css/main.3ad335d3.chunk.css"
+  },
+  {
+    "revision": "6deed0d15b932dee9a6f",
+    "url": "/polyline-to-geojson/static/js/2.75400e97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/polyline-to-geojson/static/js/2.75400e97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89c3f734ebbff7c8a7ff",
+    "url": "/polyline-to-geojson/static/js/main.eeab6873.chunk.js"
+  },
+  {
+    "revision": "d6c90508bd07d9a5285f",
+    "url": "/polyline-to-geojson/static/js/runtime-main.ab37fe4b.js"
+  },
+  {
+    "revision": "80a2dbc65ad2f9eb44dd238435c0257e",
+    "url": "/polyline-to-geojson/static/media/loading-animation.80a2dbc6.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/polyline-to-geojson/precache-manifest.3164665a58e33a92b87d128a41ae9a2b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/polyline-to-geojson/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
