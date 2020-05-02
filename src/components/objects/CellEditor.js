f03a3c5f5c558ccf51ff28595b3ebee5d3e0526c
Konrad Chilimoniuk <chilimoniuk.konrad@gmail.com>
2020-05-02 21:03:15 +0200

    Changed tooltip behaviour.

--- src/components/objects/CellEditor.js
+++ src/components/objects/CellEditor.js
@@ -2,11 +2,39 @@ import React, { Component } from 'react'
 import Tooltip from './Tooltip'
 
 export class CellEditor extends Component {
+    tdRef = React.createRef();
+    tooltipRef = React.createRef();
+
+    componentDidMount() {
+        if(this.tooltipRef.current !== null && this.tdRef.current !== null) {
+            let coordinates = {
+                left: this.tdRef.current.offsetLeft,
+                top: this.tdRef.current.offsetTop,
+                parentHeight: this.tdRef.current.offsetParent.offsetHeight
+            }
+            this.tooltipRef.current.setCoordinates(coordinates);
+        }
+    }
+
+    handleOnMouseEnter = e => {
+        e.preventDefault();
+        if(this.tooltipRef.current !== null)
+            this.tooltipRef.current.show();
+    }
+
+    handleOnMouseLeave = e => {
+        e.preventDefault();
+        if(this.tooltipRef.current !== null)
+            this.tooltipRef.current.hide();
+    }
+
     render() {
         return (
-            <td>
-                {this.props.value.length > 27 ? `${this.props.value.substring(0, 26)}...` : this.props.value}
-                {this.props.value.length > 27 && <Tooltip>{this.props.value}</Tooltip>}
+            <td ref={this.tdRef} onMouseEnter={this.handleOnMouseEnter} onMouseLeave={this.handleOnMouseLeave}>
+                {this.props.value.length > 27 ? <>
+                    {this.props.value.substring(0, 26)}...
+                    <Tooltip ref={this.tooltipRef}>{this.props.value}</Tooltip>
+                </> : this.props.value}
             </td>
         )
     }
--- src/components/objects/Tooltip.js
+++ src/components/objects/Tooltip.js
@@ -1,9 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component, createRef } from 'react'
 
 export class Tooltip extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            left: 0,
+            top: 0,
+            isVisible: false
+        }
+
+        this.tooltipRef = createRef();
+    }
+
+    setCoordinates = coordinates => {
+        let elementHeightWithOffset = this.tooltipRef.current.scrollHeight + coordinates.top - 4;
+        let left = coordinates.left - 5;
+        let top = coordinates.top - 4;
+        this.setState({
+            left: left,
+            top: coordinates.parentHeight > elementHeightWithOffset ? top : top - (elementHeightWithOffset - coordinates.parentHeight)
+        })
+    }
+
+    show = () => {
+        this.setState({ isVisible: true })
+    }
+
+    hide = () => {
+        this.setState({ isVisible: false })
+    }
+
     render() {
         return (
-            <span className="tooltip">
+            <span ref={this.tooltipRef} className={this.state.isVisible ? "tooltip show" : "tooltip"} style={{left: this.state.left, top: this.state.top}}>
                 {this.props.children}
             </span>
         )
